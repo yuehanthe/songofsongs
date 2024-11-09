a2748527485017264c0adbca2529157639bfece4
sholvoir <sovar.he@gmail.com>
2024-11-08 20:52:13 -0500

    2

---
+++ components/home.tsx
@@ -0,0 +1,9 @@
+export default () => {
+
+return <div>
+    <div class="h-6"></div>
+    <div class="text-center text-4xl text-red-600">hello world!</div>
+    <div>I need some help!</div>
+    <div></div>
+</div>
+}
