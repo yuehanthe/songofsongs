a445e1419799741344670348303f2140a9d33256
sholvoir <sovar.he@gmail.com>
2024-11-08 21:33:56 -0500

    markdown server render

--- components/home.tsx
+++ components/home.tsx
@@ -1,9 +1,17 @@
-export default () => {
+import { marked } from 'marked';
 
-return <div>
-    <div class="h-6"></div>
-    <div class="text-center text-4xl text-red-600">hello world!</div>
-    <div>I need some help!</div>
-    <div></div>
-</div>
+export default () => {
+    const x = { __html: marked.parse(`# Fresh project
+
+Your new Fresh project is ready to go. You can follow the Fresh "Getting
+Started" guide here: https://fresh.deno.dev/docs/getting-started
+
+### Usage
+            `) as string };
+    return <div>
+        <div class="h-6"></div>
+        <div class="text-center text-4xl text-red-600">hello world!</div>
+        <div>I need some help!</div>
+        <div dangerouslySetInnerHTML={x}/>
+    </div>
 }
